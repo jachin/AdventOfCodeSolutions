8e9bb0fc7d1c8ff20a7522167f7633f067c9a33a
Jachin Rupe <jrupe@goempyrean.com>
2018-12-05 14:28:03 -0600

    Some tweaks to make things run a bit faster.

--- 2018/day_5/react_polymer.py
+++ 2018/day_5/react_polymer.py
@@ -1,32 +1,45 @@
 #! /usr/bin/env python3
 
 import fileinput
+import cProfile
+from functools import lru_cache
 
 from collections import deque
 
-
+@lru_cache(maxsize=1024)
 def can_react(first, second):
-    if (first.isupper() and second.islower()) or (first.islower() and second.isupper()):
-        return first.lower() == second.lower()
+    if first.isupper():
+        if second.islower():
+            return first.lower() == second
+    else:
+        if second.isupper():
+            return first.upper() == second
     return False
 
 
 def react(polymer):
     current = ''
     previous = ''
-    for i, p in enumerate(polymer):
+    for p in polymer:
         current = p
         if can_react(previous, current):
-            return polymer[:i - 1] + polymer[i+1:]
+            (head, pattern, tail) = polymer.partition(previous + current)
+            return '{}{}'.format(head, tail)
         previous = current
     return polymer
 
 
-for line in fileinput.input():
-    polymer = line.strip()
-    while True:
-        previous_polymer = polymer
-        polymer = react(polymer)
-        if len(previous_polymer) == len(polymer):
-            print(len(previous_polymer))
-            break
+def main():
+    for line in fileinput.input():
+        polymer = line.strip()
+        while True:
+            previous_polymer = polymer
+            polymer = react(polymer)
+            if len(previous_polymer) == len(polymer):
+                print(len(previous_polymer))
+                break
+
+
+if __name__ == '__main__':
+    #cProfile.run('main()')
+    main()
