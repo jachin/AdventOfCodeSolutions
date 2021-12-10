d6c36f87e284fbc524909c00cb6da28639d75370
Jachin Rupe <jachin@jachin.rupe.name>
2021-12-09 23:59:01 -0600

    Finishing up day 6 (finally)

--- 2021/Day 6.playground/Contents.swift
+++ 2021/Day 6.playground/Contents.swift
@@ -1,55 +1,76 @@
 import Cocoa
 
 func loadStateFromFile(fileName: String) -> [Int] {
-  let fileUrl = Bundle.main.url(forResource: fileName, withExtension: "txt")!
-  let fileContents = try! String(contentsOf: fileUrl)
-  let maybeNumbers = fileContents
-    .trimmingCharacters(in: .whitespacesAndNewlines)
-    .split(separator: ",").map { Int($0) }
-  return maybeNumbers.compactMap { $0 }
+    let fileUrl = Bundle.main.url(forResource: fileName, withExtension: "txt")!
+    let fileContents = try! String(contentsOf: fileUrl)
+    let maybeNumbers = fileContents
+        .trimmingCharacters(in: .whitespacesAndNewlines)
+        .split(separator: ",").map { Int($0) }
+    return maybeNumbers.compactMap { $0 }
 }
 
 func solvePart1(days: Int, initialState: [Int]) -> Int {
-  func aDay(state: [Int]) -> [Int] {
-    var newState : [Int] = state
-    var newFish : Int = 0
-    for (i, fish) in state.enumerated() {
-      if (fish == 0) {
-        newState[i] = 6
-        newFish += 1
-      } else {
-        newState[i] = fish - 1
-      }
+    func aDay(state: [Int]) -> [Int] {
+        var newState : [Int] = state
+        var newFish : Int = 0
+        for (i, fish) in state.enumerated() {
+            if (fish == 0) {
+                newState[i] = 6
+                newFish += 1
+            } else {
+                newState[i] = fish - 1
+            }
+        }
+        return newState + Array(repeating: 8, count: newFish)
     }
-    return newState + Array(repeating: 8, count: newFish)
-  }
-  
-  var state = initialState
-  
-  for _ in 1...days {
-    state = aDay(state: state)
-  }
-  
-  return state.count
+    
+    var state = initialState
+    
+    for _ in 1...days {
+        state = aDay(state: state)
+    }
+    
+    return state.count
 }
 
 func solvePart2(days: Int, initialState: [Int]) -> Int {
-  var state = initialState
-  
-  for _ in 1...days {
-    var newFish : Int = 0
-    for (i, fish) in state.enumerated() {
-      if (fish == 0) {
-        state[i] = 6
-        newFish += 1
-      } else {
-        state[i] = fish - 1
-      }
+    var state: [Int: Int] = [:]
+    
+    // group generations
+    for f in initialState {
+        if let num = state[f] {
+            state[f] = num + 1
+        } else {
+            state[f] = 1
+        }
     }
-    state.append(contentsOf: Array(repeating: 8, count: newFish))
-  }
-  
-  return state.count
+    
+    for _ in 1...days {
+        var newState: [Int: Int] = [:]
+        var newFish = 0
+        for (age, n) in state {
+            if age == 0 {
+                newFish += n
+                if let num = newState[6] {
+                    newState[6] = num + n
+                } else {
+                    newState[6] = n
+                }
+                
+            } else {
+                if let num = newState[age - 1] {
+                    newState[age - 1] = num + n
+                } else {
+                    newState[age - 1] = n
+                }
+            }
+            
+        }
+        newState[8] = newFish
+        state = newState
+    }
+    
+    return state.values.reduce(0, +)
 }
 
 let exampleData = loadStateFromFile(fileName: "example")
@@ -58,6 +79,6 @@ let exampleData = loadStateFromFile(fileName: "example")
 
 let puzzleData = loadStateFromFile(fileName: "puzzle_input")
 //solvePart1(days: 80, initialState: puzzleData)
-//solvePart2(days: 256, initialState: exampleData)
-print(solvePart2(days: 256, initialState: puzzleData))
+solvePart2(days: 256, initialState: exampleData)
+solvePart2(days: 256, initialState: puzzleData)
 
