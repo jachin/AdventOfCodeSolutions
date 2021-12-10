250d1daeaa37ecc311a18ba7e1f2262be1250e10
Jachin Rupe <jachin@jachin.rupe.name>
2021-12-10 01:02:48 -0600

    Day 7 part 2

--- 2021/Day 7.playground/Contents.swift
+++ 2021/Day 7.playground/Contents.swift
@@ -10,7 +10,6 @@ func loadStateFromFile(fileName: String) -> [Int] {
 }
 
 func part1(initialState: [Int]) -> Int {
-    
     let highest = initialState.max(by: <)!
     let lowest = initialState.min(by: <)!
     var best: Int = Int.max
@@ -25,5 +24,29 @@ func part1(initialState: [Int]) -> Int {
     return best
 }
 
-part1(initialState: loadStateFromFile(fileName: "example"))
-part1(initialState: loadStateFromFile(fileName: "puzzle_input"))
+func part2(initialState: [Int]) -> Int {
+    func sumUp (n: Int) -> Int {
+        if (n < 2_) {
+            return 1
+        }
+        return Array(1...n).reduce(0, +)
+    }
+    
+    let highest = initialState.max(by: <)!
+    let lowest = initialState.min(by: <)!
+    var best: Int = Int.max
+    for height in lowest...highest {
+        let fuelCost = initialState.map { sumUp(n: abs($0 - height)) }.reduce(0, +)
+        if fuelCost < best {
+            best = fuelCost
+        }
+    }
+    
+    return best
+}
+
+//part1(initialState: loadStateFromFile(fileName: "example"))
+//part1(initialState: loadStateFromFile(fileName: "puzzle_input"))
+
+part2(initialState: loadStateFromFile(fileName: "example"))
+part2(initialState: loadStateFromFile(fileName: "puzzle_input"))
