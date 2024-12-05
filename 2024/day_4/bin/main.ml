3f6dad3a2087792a20fde1555ee552c7d3fda5f0
Jachin Rupe <jachin@jachin.rupe.name>
2024-12-05 15:12:04 -0600

    Got day 4 - part 1 done... at last.

--- 2024/day_4/bin/main.ml
+++ 2024/day_4/bin/main.ml
@@ -10,62 +10,57 @@ let read_file filename =
 let chars_to_string chars =
   String.concat "" (List.map (String.make 1) chars)
 
+let get_chars_to_the_right row col matrix =
+  Matrix.get_line matrix row col row (col + 3)
+
+let get_chars_going_down row col matrix =
+  Matrix.get_line matrix row col (row + 3) col
+
+let get_chars_going_diagonal_down_and_right row col matrix =
+  Matrix.get_line matrix row col (row + 3) (col + 3)
+
+let get_chars_going_diagonal_down_and_left row col matrix =
+  Matrix.get_line matrix row col (row + 3) (col - 3)
+
+
 let generate_perms matrix =
   let perms = BatDynArray.create () in
   Matrix.iter (fun cords _ ->
     let (row, col) = cords in
-    match Matrix.get_line matrix row col row (col + 3) with
-      | Some line ->
-        BatDynArray.add perms (line |> chars_to_string);
-        BatDynArray.add perms (line |> List.rev |> chars_to_string);
-        ()
-      | None ->
-        ();
-
-    match Matrix.get_line matrix row col (row + 3) col with
-      | Some line ->
-        BatDynArray.add perms (line |> chars_to_string);
-        BatDynArray.add perms (line |> List.rev |> chars_to_string);
-        ()
-      | None ->
-        ();
-
-    match Matrix.get_line matrix row col (row + 3) (col + 3) with
-      | Some line ->
-        BatDynArray.add perms (line |> chars_to_string);
-        BatDynArray.add perms (line |> List.rev |> chars_to_string);
-        ()
-      | None ->
-        ();
-
-    match Matrix.get_line matrix row col (row - 3) (col + 3) with
-      | Some line ->
-        print_endline ("diag - " ^ (line |> chars_to_string));
-        BatDynArray.add perms (line |> chars_to_string);
-        BatDynArray.add perms (line |> List.rev |> chars_to_string);
-        ()
-      | None ->
-        ();
+    [
+        get_chars_to_the_right row col matrix;
+        get_chars_going_down row col matrix;
+        get_chars_going_diagonal_down_and_right row col matrix;
+        get_chars_going_diagonal_down_and_left row col matrix
+      ]
+        |> List.map Option.to_list
+        |> List.concat
+        |> List.iter (fun line ->
+          BatDynArray.add perms (line |> chars_to_string);
+          BatDynArray.add perms (line |> List.rev |> chars_to_string);
+        )
     ) matrix;
   BatDynArray.to_list perms
 
-let print_perms perms =
+(* let print_perms perms =
   List.iter (fun perm ->
     Printf.printf "%s\n" perm
   ) perms;
-  perms
+  perms *)
 
 (* Part 1 test *)
 let () = read_file "data/example.txt"
   |> Matrix.build_from_string
-  (* |> Matrix.iter (fun cords char ->
-    match cords with
-    | (col, row) ->
-      Printf.printf "(%i, %i) %c\n" col row char
-  ) *)
   |> generate_perms
-  |> (fun perms -> List.length perms |> string_of_int |> print_endline; perms )
-  |> print_perms
+  |> List.filter (fun p -> p = "XMAS")
+  |> List.length
+  |> string_of_int
+  |> print_endline
+
+(* Part 1 *)
+let () = read_file "data/input.txt"
+  |> Matrix.build_from_string
+  |> generate_perms
   |> List.filter (fun p -> p = "XMAS")
   |> List.length
   |> string_of_int
